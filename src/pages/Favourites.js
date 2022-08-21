e1b0fc0f141880e2e2155c23815c9dc3495302a1
SolomonOjigbo <solomonojigbo@gmail.com>
2022-08-21 08:45:39 +0100

    Configure the favourites page to display list of Favourites

--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -1,5 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import EventList from "../components/events/EventList";
+import FavouritesContext from "../store/favouritesContext";
 
 export default function Favourites() {
-	return <div>Favourites</div>;
+	const favouriteContext = useContext(FavouritesContext);
+
+	return (
+		<section>
+			<h2>Favourites</h2>
+			<EventList meetupEvents={favouriteContext.favourites} />
+		</section>
+	);
 }
