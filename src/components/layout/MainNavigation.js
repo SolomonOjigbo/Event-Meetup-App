10c4ac7a356c7e0db0156e8abe7f823e022fc8e1
SolomonOjigbo <solomonojigbo@gmail.com>
2022-08-25 20:50:03 +0100

    Added Favourite count badge to the main Navigation and refactored the favourite component to render favourite content conditionally

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import FavouritesContext from "../../store/favouritesContext";
 import classes from "./MainNavigation.module.css";
 
 export default function MainNavigation() {
+	const favouritesContext = useContext(FavouritesContext);
+
 	return (
 		<header className={classes.header}>
 			<div className={classes.logo}>Event Meetups</div>
@@ -15,7 +18,12 @@ export default function MainNavigation() {
 						<NavLink to="/new-meetup">New Meetup</NavLink>
 					</li>
 					<li>
-						<NavLink to="/favourites">My Favourites</NavLink>
+						<NavLink to="/favourites">
+							My Favourites
+							<span className={classes.badge}>
+								{favouritesContext.totalFavourites}{" "}
+							</span>
+						</NavLink>
 					</li>
 				</ul>
 			</nav>
--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -5,10 +5,18 @@ import FavouritesContext from "../store/favouritesContext";
 export default function Favourites() {
 	const favouriteContext = useContext(FavouritesContext);
 
+	let content;
+
+	if (favouriteContext.totalFavourites === 0) {
+		content = <p>Sorry, you have no favourites yet. Start adding some!</p>;
+	} else {
+		content = <EventList meetupEvents={favouriteContext.favourites} />;
+	}
+
 	return (
 		<section>
 			<h2>Favourites</h2>
-			<EventList meetupEvents={favouriteContext.favourites} />
+			{content}
 		</section>
 	);
 }
